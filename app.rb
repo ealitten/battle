55314154af56a2932f976c3e272cd4a525fc2c3a
ealitten <ea.litten@gmail.com>
2018-01-16 16:14:21 +0000

    Refactor for post-redirect-get

--- app.rb
+++ app.rb
@@ -2,16 +2,24 @@ require 'sinatra/base'
 
 class Battle < Sinatra::Base
 
+  enable :sessions
+  set :session_secret, '5ee6a1127fa9ae1dd477dec9a93fcae15d8713e59e1e67ae5ac1e643f6bdd9132229287405c1bb65996a5e6c850a3111997b3110d5d3326f1abb0bb9414ed8d9'
+
   get '/' do
     erb(:index)
   end
 
   post '/names' do
-    @player_1 = params[:player_1]
-    @player_2 = params[:player_2]
-    erb(:play)
+    session[:player_1_name] = params[:player_1]
+    session[:player_2_name] = params[:player_2]
+    redirect '/play'
   end
 
+  get '/play' do
+    @player_1 = session[:player_1_name]
+    @player_2 = session[:player_2_name]
+    erb(:play)
+  end
 
   # start server if ruby file run directly
   run! if app_file == $0
