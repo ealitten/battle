a49d3c2a7d888a13ffb671935f1d0512fd2ae251
Agata <agata.golebiewska@yahoo.co.uk>
2018-01-16 15:10:29 +0000

    Add enter players form

--- app.rb
+++ app.rb
@@ -1,14 +1,19 @@
 require 'sinatra/base'
 
-
 class Battle < Sinatra::Base
 
   get '/' do
-    "Testing infrastructure working!"
+    erb(:index)
   end
+
+  post '/names' do
+    @player_1 = params[:player_1]
+    @player_2 = params[:player_2]
+    erb(:play)
+  end
+
 
   # start server if ruby file run directly
   run! if app_file == $0
 
 end
-
---
+++ spec/features/battle_spec.rb
@@ -0,0 +1,12 @@
+
+
+feature 'Filling in names' do
+  scenario 'Players enter their names and see them on screen' do
+    visit('/')
+    fill_in "player_1", :with => "Ed"
+    fill_in "player_2", :with => "Agata"
+    click_button "Begin the Battle!"
+    expect(page).to have_text("Player 1: Ed")
+    expect(page).to have_text("Player 2: Agata")
+  end
+end
