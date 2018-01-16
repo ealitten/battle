03e5b40b2784ed24fd33140cda8334108eb509d5
ealitten <ea.litten@gmail.com>
2018-01-16 14:36:48 +0000

    Setup testing infrastructure

--- app.rb
+++ app.rb
@@ -1,11 +1,13 @@
 require 'sinatra/base'
 
+
 class Battle < Sinatra::Base
 
   get '/' do
-    "Hello Battle!"
+    "Testing infrastructure working!"
   end
 
+  # start server if ruby file run directly
   run! if app_file == $0
 
 end
--- config.ru
+++ config.ru
@@ -1,4 +1,4 @@
 require 'rubygems'
-require './lib/app.rb'
+require './app.rb'
 
 run Battle
