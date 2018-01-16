1b06f8b8a6c5c4b288c8b5309b65072dcb05f243
Agata <agata.golebiewska@yahoo.co.uk>
2018-01-16 16:31:27 +0000

    Add HP for Player 2

--- spec/features/battle_spec.rb
+++ spec/features/battle_spec.rb
@@ -1,5 +1,3 @@
-
-
 feature 'Filling in names' do
   scenario 'Players enter their names and see them on screen' do
     visit('/')
@@ -8,5 +6,15 @@ feature 'Filling in names' do
     click_button "Begin the Battle!"
     expect(page).to have_text("Player 1: Ed")
     expect(page).to have_text("Player 2: Agata")
+  end
+end
+
+feature 'Show HP for each player' do
+  scenario 'Display HP on the screen' do
+    visit('/')
+    fill_in "player_1", :with => "Ed"
+    fill_in "player_2", :with => "Agata"
+    click_button "Begin the Battle!"
+    expect(page).to have_text("Player 2 HP: 100")
   end
 end
